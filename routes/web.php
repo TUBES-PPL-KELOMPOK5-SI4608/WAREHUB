25bc23064e9b9140d7d5c86dcd102fecbf1baf23
anykindofnaa <nazasapinath30@gmail.com>
2025-04-20 16:00:53 +0700

    back to original

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-Route::get('/test/1', function () {
-=======
-Route::get('/test/2', function () {
->>>>>>> 2703fa1ce7a3e063cf2ada9e9a86a0bf966873a0
-    return view('welcome');
-});
